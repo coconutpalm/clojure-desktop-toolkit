b8c24df83ae7b0f43588e3a10512381def985cda
David Orme <djo@coocnut-palm-software.com>
2025-07-16 22:58:51 -0500

    Updated for ARM MacOS

--- examples/starter/src/starter/main.clj
+++ examples/starter/src/starter/main.clj
@@ -17,8 +17,11 @@
   (binding [*repl* true]           ; Allow add-libs from Clojure 1.12 to work.
     (require '[starter.ui])        ; Actually initialize SWT with the correct platform-specific lib and generate the SWT Clojure API.
 
-    (when-let [splash-screen (SplashScreen/getSplashScreen)]
-      (.close splash-screen))      ; Close the splash screen if it's being displayed
+    ;; Close the splash screen if it's being displayed; on MacOS this must happen on a separate thread
+    (future
+      (Thread/sleep 1000)  ; Wait a second to allow the splash screen to be displayed on MacOS
+      (when-let [splash-screen (SplashScreen/getSplashScreen)]
+        (.close splash-screen)))
 
     (eval                          ; Start the user interface.
      `(starter.ui/hello ~@args)))) ; We have to `eval` because the `require` isn't in the `ns` form; the compiler hates us otherwise.  LOL.
