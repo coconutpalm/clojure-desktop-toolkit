ed6c4eaadeea41e64f9aec9928e02b1b098b3236
David Orme <djo@coconut-palm-software.com>
2026-01-25 21:17:16 -0500

    Delete m2 repository

--- newRelease.sh
+++
@@ -1,3 +0,0 @@
-bash -c "source scrape-swt.sh; set -e -x; stage_2_download"
-bash -c "source scrape-swt.sh; set -e -x; stage_3_extract"
-bash -c "source scrape-swt.sh; set -e -x; stage_4_install"
---
+++ swt-native-libs/README.md
@@ -0,0 +1,4 @@
+# SWT
+
+This directory contains the Eclipse Platform's SWT downloads for all platforms for the
+specified version.
